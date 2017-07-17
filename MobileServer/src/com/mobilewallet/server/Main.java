730cdc918f57e26921a2453268052c56d35f8acc
SajuJayson <saju.jayson@gmail.com>
2017-07-16 21:39:11 -0400

    Main Updated

--- MobileServer/src/com/mobilewallet/server/Main.java
+++ MobileServer/src/com/mobilewallet/server/Main.java
@@ -3,6 +3,14 @@
  */
 package com.mobilewallet.server;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @author Saju
  *
@@ -12,9 +20,29 @@ public class Main {
 	/**
 	 * @param args
 	 */
+	private static final int portnumber = 60123;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+ServerSocket serverSocket = null;
+try{
+	System.out.println("Server starting at port number: "+portnumber);
+	serverSocket = new ServerSocket();
+	
+	System.out.println("waiting for clients to connect.");
+Socket socket = serverSocket.accept();
 
+BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+bw.write("This is a message from the client");
+bw.newLine();
+bw.flush();
+
+BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+System.out.println("Message from server : "+br.readLine());
+
+
+}
+catch(IOException e){
+	e.printStackTrace();
+}
 	}
 
 }
