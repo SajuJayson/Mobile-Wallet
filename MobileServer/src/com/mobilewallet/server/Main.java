31ede434b1332553e007ffaa21b8c2578c195e58
SajuJayson <saju.jayson@gmail.com>
2017-07-16 21:18:04 -0400

    ServerProject
    Server Project created

---
+++ MobileServer/src/com/mobilewallet/server/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.mobilewallet.server;
+
+/**
+ * @author Saju
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- MobileWallet/app/src/main/java/com/comp231/saju/mobilewallet/MainActivity.java
+++ MobileWallet/app/src/main/java/com/comp231/saju/mobilewallet/MainActivity.java
@@ -2,12 +2,42 @@ package com.comp231.saju.mobilewallet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String hostname = "localhost";
+    private static final int portnumber = 60123;
+    private static final String debugString = "debug";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Socket socket = null;
+        try{
+            Log.i(debugString,"Attempting to connect to server");
+            socket = new Socket(hostname, portnumber);
+            Log.i(debugString,"Connection Established");
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bw.write("This is a message from the server");
+            bw.newLine();
+            bw.flush();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println("Message from client : "+br.readLine());
+
+        }catch(IOException e){
+            Log.e(debugString, e.getMessage());
+        }
+
     }
 }
