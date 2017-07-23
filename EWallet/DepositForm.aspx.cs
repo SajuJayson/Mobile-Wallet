eca374060f3c38a4efb52666a2f0c1dacdc48dab
LovedeepKaur42 <kaurlovedeep108@gmail.com>
2017-07-23 16:43:44 -0700

    calender

--- EWallet/DepositForm.aspx.cs
+++ EWallet/DepositForm.aspx.cs
@@ -9,7 +9,10 @@ public partial class DepositForm : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if(!IsPostBack)
+        {
+            Calendar1.Visible =false;
+        }
     }
 
     protected void BtnConfirmDeposit_Click(object sender, EventArgs e)
@@ -20,5 +23,23 @@ public partial class DepositForm : System.Web.UI.Page
     protected void BtnBack_Click(object sender, EventArgs e)
     {
         Response.Redirect("Default.aspx");
+    }
+
+    protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+    {
+        if (Calendar1.Visible == false)
+        {
+            Calendar1.Visible = true;
+        }
+        else
+        {
+            Calendar1.Visible = false;
+        }
+    }
+
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+        TxtBoxExDate.Text =Convert.ToString (Calendar1.SelectedDate);
+        Calendar1.Visible = false;
     }
 }
