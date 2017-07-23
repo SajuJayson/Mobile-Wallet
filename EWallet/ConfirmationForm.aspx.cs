2ecb3b4b11de87c41e0dc7dd01f2e88995e8a6f9
SajuJayson <saju.jayson@gmail.com>
2017-07-23 10:47:47 -0400

    Entity framework and Database connection
    Entity framework has been used to connect to a sql database file

--- EWallet/ConfirmationForm.aspx.cs
+++ EWallet/ConfirmationForm.aspx.cs
@@ -7,9 +7,12 @@ using System.Web.UI.WebControls;
 
 public partial class ConfirmationForm : System.Web.UI.Page
 {
+
+    eWalletEntities dbcontext = new eWalletEntities();
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        LabelCurrentBalance.Text = "You are a Billionaire";
+        LabelUpdatedBalance.Text= dbcontext.AccountInfoes.ToList().ToString();
     }
 
     protected void Button1_Click(object sender, EventArgs e)
