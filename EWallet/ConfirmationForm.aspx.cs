e73f64bbc0a0bc482347f58cb0c372b1b585fee8
SajuJayson <saju.jayson@gmail.com>
2017-07-28 04:33:10 -0400

    Revert "Revert "Story 1 - Complete""
    This reverts commit 3b4f3c31e14c2c50ef1d74beec1c00494399ac0a.

--- EWallet/ConfirmationForm.aspx.cs
+++ EWallet/ConfirmationForm.aspx.cs
@@ -11,6 +11,10 @@ public partial class ConfirmationForm : System.Web.UI.Page
     
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            ViewState["RefUrl"] = Request.UrlReferrer.ToString();
+        }
         LabelCurrentBalance.Text = Request.QueryString["CurBal"];
         LabelAmountDeducted.Text= Request.QueryString["DeductedAmt"];
         LabelUpdatedBalance.Text = Request.QueryString["updatedAmt"];
@@ -21,6 +25,22 @@ public partial class ConfirmationForm : System.Web.UI.Page
         DataBaseHandler cls = new DataBaseHandler();
         //Convert.ToInt16(Session["Userid"])
         int result =cls.TransferMoney(1003, Convert.ToInt16(Request.QueryString["CustID"]), Convert.ToDouble(Request.QueryString["DeductedAmt"]));
-        Response.Redirect("AcknowledgementForm.aspx?RemBal="+ Request.QueryString["updatedAmt"] + "&CustID="+ Request.QueryString["CustID"]);
+        if (result == 0)
+        {
+            Response.Redirect("AcknowledgementForm.aspx?RemBal=" + Request.QueryString["updatedAmt"] + "&CustID=" + Request.QueryString["CustID"]+"&Status=Fail");
+
+        }
+        else
+        {
+            Response.Redirect("AcknowledgementForm.aspx?RemBal=" + Request.QueryString["updatedAmt"] + "&CustID=" + Request.QueryString["CustID"] + "&Status=Pass");
+
+        }
+    }
+
+    protected void BtnConfirm_Click(object sender, EventArgs e)
+    {
+        object refUrl = ViewState["RefUrl"];
+        if (refUrl != null)
+            Response.Redirect((string)refUrl);
     }
 }
