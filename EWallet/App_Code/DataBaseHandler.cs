578d3034a7deaaa664a90b506409c2406923ff2d
SajuJayson <saju.jayson@gmail.com>
2017-08-12 16:59:10 -0400

    Withdrawal and Depsoit Sp and Methods

--- EWallet/App_Code/DataBaseHandler.cs
+++ EWallet/App_Code/DataBaseHandler.cs
@@ -30,6 +30,32 @@ public class DataBaseHandler
     }
 
 
+    public int WithdrawMoney(int customerID, int BankAccountNo, double Amount)
+    {
+        conn.Open();
+        //SqlCommand command = new SqlCommand("update transaction set CustomerId='" + SenderID + "', BenificaryID='" + BenificaryID + "', TransactionTime=getdate() Amount='" + Amount.ToString() + "', TransactionType='T'", conn);
+        SqlCommand cmd = new SqlCommand("Withdrawal", conn);
+        cmd.Parameters.Add("@CusId", SqlDbType.Int).Value = customerID;
+        cmd.Parameters.Add("@AccNo", SqlDbType.Int).Value = BankAccountNo;
+        cmd.Parameters.Add("@Amount", SqlDbType.Money, 100).Value = Amount;
+        cmd.CommandType = CommandType.StoredProcedure;
+        int result = cmd.ExecuteNonQuery();
+        return result;
+    }
+
+    public int DepositMoney(int customerID, int BankAccountNo, double Amount)
+    {
+        conn.Open();
+        //SqlCommand command = new SqlCommand("update transaction set CustomerId='" + SenderID + "', BenificaryID='" + BenificaryID + "', TransactionTime=getdate() Amount='" + Amount.ToString() + "', TransactionType='T'", conn);
+        SqlCommand cmd = new SqlCommand("Deposit", conn);
+        cmd.Parameters.Add("@CusId", SqlDbType.Int).Value = customerID;
+        cmd.Parameters.Add("@AccNo", SqlDbType.Int).Value = BankAccountNo;
+        cmd.Parameters.Add("@Amount", SqlDbType.Money, 100).Value = Amount;
+        cmd.CommandType = CommandType.StoredProcedure;
+        int result = cmd.ExecuteNonQuery();
+        return result;
+    }
+
     public DataSet UserDetails(String Customer, int flag)
     {
         SqlCommand cmd = new SqlCommand("GetUserDetail", conn);
